a49159916a5edf5198294fde968647c58fb8e1d4
Dmitry <agressor198@mail.ru>
2020-07-25 14:46:50 +0300

    v 3.1 creating base test class

--- src/test/java/page/ConfirmationPage.java
+++ src/test/java/page/ConfirmationPage.java
@@ -4,16 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class ConfirmationPage {
-    WebDriver driver = DriverSingleton.getDriver();
+public class ConfirmationPage extends BasePage{
+    String pageURL = "https://idemo.bspb.ru/auth/otp?authOptionId=SMS%3A10005";
     By confirmationWindow = By.xpath("//*[@id=\"login-form\"]/div[2]/div");
 
 
-    public  WebElement lookForElement(By by){
-        return driver.findElement(by);
-    }
-
     public boolean isDisplayed(){
         return lookForElement(confirmationWindow).isDisplayed();
+    }
+    @Override
+    public void open() {
+        driver.get(pageURL);
     }
 }
--- src/test/java/page/SigninPage.java
+++ src/test/java/page/SigninPage.java
@@ -5,17 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class SigninPage {
-    WebDriver driver = DriverSingleton.getDriver();
-
+public class SigninPage extends BasePage{
     String pageURL = "https://idemo.bspb.ru/";
     By usernameInput = By.name("username");
     By passwordInput = By.name("password");
     By signInButton =  By.id("login-button");
-
-    public WebElement lookForElement(By by) {
-        return driver.findElement(by);
-    }
 
     public void insertLogin(String login) {
         lookForElement(usernameInput).sendKeys(login);
@@ -31,7 +25,7 @@ public class SigninPage {
         lookForElement(signInButton).click();
 
     }
-
+    @Override
     public void open() {
         driver.get(pageURL);
 
