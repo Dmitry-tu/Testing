8924fb33843dcc982221f8ecad0fcd8cd6403e82
Dmitry <agressor198@mail.ru>
2020-07-25 16:22:30 +0300

    v 3.2 non static

--- src/test/java/page/ConfirmationPage.java
+++ src/test/java/page/ConfirmationPage.java
@@ -1,8 +1,6 @@
 package page;
-import Driver.DriverSingleton;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class ConfirmationPage extends BasePage{
     String pageURL = "https://idemo.bspb.ru/auth/otp?authOptionId=SMS%3A10005";
--- src/test/java/page/SigninPage.java
+++ src/test/java/page/SigninPage.java
@@ -1,22 +1,19 @@
 package page;
 
-import Driver.DriverSingleton;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class SigninPage extends BasePage{
-    static String pageURL = "https://idemo.bspb.ru/";
-    static By usernameInput = By.name("username");
-    static By passwordInput = By.name("password");
+     String pageURL = "https://idemo.bspb.ru/";
+    By usernameInput = By.name("username");
+    By passwordInput = By.name("password");
     static By signInButton =  By.id("login-button");
 
-    public static void insertLogin(String login) {
+    public void insertLogin(String login) {
         lookForElement(usernameInput).sendKeys(login);
 
     }
 
-    public static void insertPassword(String password) {
+    public void insertPassword(String password) {
         lookForElement(passwordInput).sendKeys(password);
 
     }
