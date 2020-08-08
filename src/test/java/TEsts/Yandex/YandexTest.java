c56e56b4027b7f6d2ff744b1cad2f31eda08c24e
Dmitry <agressor198@mail.ru>
2020-08-08 09:53:04 +0300

    v 5 highest fullest version 1.2(More tests)

--- src/test/java/TEsts/Yandex/YandexTest.java
+++ src/test/java/TEsts/Yandex/YandexTest.java
@@ -26,7 +26,7 @@ public class YandexTest extends BaseTest {
     @Description("In this test we will login with correct data.When we logged we see our account screen")
     @Story("Test for login with valid data")
     @Test
-    public void successfulLoginTest() throws InterruptedException {
+    public void successfulLoginTest()  {
         yandexMusicMain.CheckAuthorisation();
         yandexMusicLogin.UserNameClickEnter("Testomatio");
         yandexMusicLogin.PasswordNameClickEnter("Test.stay.strong");
@@ -35,4 +35,32 @@ public class YandexTest extends BaseTest {
 
 
     }
+    @Epic("Testing for https://music.yandex.ru/home")
+    @Feature(value = "Unsuccessful Login with empty login field")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("In this test we will login without login .")
+    @Story("Test for login with valid data")
+    @Test
+    public void UnsuccessfulLoginTestWithEmptyFieldLogin(){
+        yandexMusicMain.CheckAuthorisation();
+        yandexMusicLogin.UserNameClickEnter("");
+        yandexMusicLogin.PasswordNameClickEnter("Test.stay.strong");
+        yandexMusicMain.ClickIcon();
+        yandexMusicMain.CompareUserName("Testomatio");
+    }
+
+    @Epic("Testing for https://music.yandex.ru/home")
+    @Feature(value = "Unsuccessful Login with empty Password field")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("In this test we will login without password.")
+    @Story("Test for login with valid data")
+    @Test
+    public void UnsuccessfulLoginTestWithEmptyFieldPassword(){
+        yandexMusicMain.CheckAuthorisation();
+        yandexMusicLogin.UserNameClickEnter("Testomatio");
+        yandexMusicLogin.PasswordNameClickEnter("");
+        yandexMusicMain.ClickIcon();
+        yandexMusicMain.CompareUserName("Testomatio");
+    }
+
 }
--- src/test/java/page/Yandex/YandexMusicMain.java
+++ src/test/java/page/Yandex/YandexMusicMain.java
@@ -8,7 +8,14 @@ import static page.Yandex.YandexMusicMainLocators.*;
 public class YandexMusicMain extends BasePage {
     String pageURl = "https://music.yandex.ru/home";
 
-    @Step(value = "Authorisation on")
+
+    @Step(value = "Open page of Web site")
+    public void open() {
+        super.open(pageURl);
+
+    }
+
+    @Step(value = "Choose button(Log In) on web site and ")
     public void CheckAuthorisation() {
         windowHandle(0);
         lookForElement(inButton).click();
@@ -16,18 +23,14 @@ public class YandexMusicMain extends BasePage {
 
     }
 
-    public void open() {
-        super.open(pageURl);
-
-    }
-
+    @Step(value = "Click icon of our account")
     public void ClickIcon() {
         windowHandle(0);
         lookForElement(inButtonLabel).click();
 
 
     }
-
+    @Step(value = "Compare account name with our data(to understand on our account we is or not)")
     public void CompareUserName(String text) {
         getText(accountName, text);
 
